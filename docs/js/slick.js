aed3c7a48689b88e281af12c9c45719c71af4992
teru1012 <kawamurateruki@gmail.com>
2025-01-22 14:24:09 +0900

    スライドショーの編集

--- docs/js/slick.js
+++ docs/js/slick.js
@@ -4,12 +4,12 @@ $('.lis-slide').slick({
 	autoplay: true,
 	dots: false,				//丸いページナビボタンを表示
 	arrows: false,			//左右の矢印
-	Speed: 100000,	//切り替えのスピード。
+	Speed: 5000,	//切り替えのスピード。
 	cssEase: 'linear',
 	slidesToShow: 2,
 	slidesToScroll:1,
-
-
+  variableWidth: false, // falseでスライド幅が均等
+  infinite: true,
    //画面の幅が890px以下の場合//
    responsive: [
    {
